716a3ca743d0007abe26871b5f3aed5217e044a6
Parikshit Singh Rathore <54709254+parikshit14@users.noreply.github.com>
2021-04-16 10:54:49 +0530

    Black formattted (#2)
    * updating
    
    * black formatted the code
    
    * updating black formatted code

--- cancer_detection_python_code.py
+++ cancer_detection_python_code.py
@@ -13,23 +13,23 @@ Created on Tue Mar 26 09:26:23 2019
 """
 
 import numpy as np
-from sklearn import preprocessing , model_selection , naive_bayes
+from sklearn import preprocessing, model_selection, naive_bayes
 import pandas as pd
 from sklearn.metrics import accuracy_score
 
-df=pd.read_csv('cancer_data.txt')
-df.replace('?',-99999,inplace=True)
-df.drop('id',1,inplace=True)
+df = pd.read_csv("cancer_data.txt")
+df.replace("?", -99999, inplace=True)
+df.drop("id", 1, inplace=True)
 
-x=np.array(df.drop(['class'],1))
-y=np.array(df['class'])
+x = np.array(df.drop(["class"], 1))
+y = np.array(df["class"])
 
-x_train,x_test,y_train,y_test=model_selection.train_test_split(x,y,test_size=0.2)
+x_train, x_test, y_train, y_test = model_selection.train_test_split(x, y, test_size=0.2)
 
-clf=naive_bayes.GaussianNB()
-clf.fit(x_train,y_train)
-pre=clf.predict(x_test)
+clf = naive_bayes.GaussianNB()
+clf.fit(x_train, y_train)
+pre = clf.predict(x_test)
 
-acc=accuracy_score(pre,y_test)
+acc = accuracy_score(pre, y_test)
 print(pre)
 print(acc)
